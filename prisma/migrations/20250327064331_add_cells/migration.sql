ccd9fde6d515891aab1dede81374494f7638cbe0
Kosyrev143 <artemkosyrev93@gmail.com>
2025-03-27 11:47:12 +0500

    feat: () add prisma

---
+++ prisma/migrations/20250327064331_add_cells/migration.sql
@@ -0,0 +1,33 @@
+/*
+  Warnings:
+
+  - You are about to drop the `users` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE `users`;
+
+-- CreateTable
+CREATE TABLE `tables` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `cells` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `table_id` INTEGER NOT NULL,
+    `row` INTEGER NOT NULL,
+    `column` INTEGER NOT NULL,
+    `value` VARCHAR(191) NOT NULL,
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `cells` ADD CONSTRAINT `cells_table_id_fkey` FOREIGN KEY (`table_id`) REFERENCES `tables`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
